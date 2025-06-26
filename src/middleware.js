5b13d25830b782fab3b17f09c528372b4877a10f
Rancune <pitet.brian@gmail.com>
2025-06-27 01:40:42 +0200

    authentification clerk de marde.

--- src/middleware.js
+++ src/middleware.js
@@ -7,13 +7,23 @@ export default clerkMiddleware({
   publicRoutes: [
     // Routes des crons qui ont leur propre authentification
     "/api/crons/(.*)",
+    "/landing-page",
+    "/api/public-route",
+    "/api/auth/callback",
+    "/api/auth/sign-in",
+    "/api/auth/sign-up",
+    "/api/auth/sign-out",
+    "/api/auth/sign-in/callback",
   ],
 
   // Spécifiez l'URL de connexion pour que la redirection automatique
   // fonctionne correctement avec votre sous-domaine Clerk
   signInUrl: "https://accounts.fixer.rancune.games/sign-in",
+  frontendApi: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API,
 });
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+
